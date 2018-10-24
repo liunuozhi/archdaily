7012672df33f71c1d6eb30f4b44ef91fbe1c100f
liunuozhi <liunuozhi1995@gmail.com>
2018-10-24 11:34:10 +0800

    turn to next page

--- archdaily/archdaily/spiders/archdailyCrawler.py
+++ archdaily/archdaily/spiders/archdailyCrawler.py
@@ -6,24 +6,28 @@ from pprint import pprint
 
 class ArchdailyCrawl(scrapy.Spider):
     name = 'archdaily'
-
     start_urls = [
         'https://www.archdaily.com/search/projects/categories/theaters-and-performance'
     ]
 
-    # TODO turn to next page
+    # TODO Priority test exported json file whether can be opened by pandas
+    # TODO better to have IP pools
 
     def parse(self, response):
 
-        # TODO Use xpath to find all project link
+        # TODO Use xpath to find all category link
         BASE_URL = 'https://www.archdaily.com{0}'
         soup = BeautifulSoup(response.body, 'lxml')
-        category_page = []
+        category_page = [] # project content page url collection
         for tag in soup.find_all('a', attrs={'class': 'afd-search-list__link'}):
             link = BASE_URL.format(tag['href'])
             category_page.append(link)
         for url in category_page:
             yield Request(url, callback=self.visit_project_page)
+
+        next_url = response.css('a.next::attr(href)').extract_first()
+        if next_url:
+            yield Request(BASE_URL.format(next_url), callback=self.parse)
 
     def visit_project_page(self, response):
         soup = BeautifulSoup(response.body, 'lxml')
@@ -41,5 +45,3 @@ class ArchdailyCrawl(scrapy.Spider):
         item['article'] = [p.text.strip() for p in soup.body.article.find_all('p')]
 
         yield item
-
-
